495f7ae0778b36c0ef2e6476ed0ede9a6c3ac828
Mohammed Cherigui <mohammed29.cherigui@gmail.com>
2024-12-02 19:47:46 +0100

    solved

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	g++ test2.cpp -o test2
+	./test2
--- test2.cpp
+++ test2.cpp
@@ -30,10 +30,31 @@ void Rectangle::print(){
 }
 void Rectangle::draw(){
     // draw the rectangle using | and -
+    if (m_height == m_width)
+        std::cout << "It is not a rectangle" << '\n';
+    else
+    {
+        for (int i = 0; i < m_width; i++)
+            std::cout << "-";  
+        std::cout << '\n';
+          
+        for (int j = 0; j < m_height; j++)
+        {
+            std::cout << "|";
+            for (int k = 0; k < m_width - 1; k++)
+                std::cout << " ";
+            std::cout << "|";
+            std::cout << '\n';
+        }
+        for (int i = 0; i < m_width; i++)
+            std::cout << "-"; 
+        std::cout << '\n';  
+    }
 }
 int main(){
     Rectangle my_rect(42,123, "big rectangle");
     Rectangle small(4,12, "small boy");
-    my_rect.print();
-    small.print();
+    // my_rect.print();
+    // small.print();
+    small.draw();
 }
